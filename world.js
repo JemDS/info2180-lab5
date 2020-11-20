a29d5c86004315204b99fe591283a68f9ef5eae4
Deidre-Ann <d_jemison@hotmail.com>
2020-11-20 00:37:05 -0500

    Updated

--- world.js
+++ world.js
@@ -11,28 +11,21 @@ $(document).ready(function()
         e.preventDefault();
         console.log('clicked')
 
-        $('#country').on('keyup', function(){
-            var searchCou = $(this).val();
-        
-
-            $.ajax({
-                url: "http://localhost/info2180-lab5/world.php",
-                method: "POST",
-                data: {country:searchCou},
-                success: function(data){
-                    $("#result").html(data);
-                }
-            })
-
-            //.done(function(response){
-            //    result.html(response);
-            })
-
-            .fail(function(){
-                alert('There was a problem loading the database!')
-            });  
-
+        $('#country').keyup(function(){
+            var search = $(this).val();
         })
+
+        $.ajax("http://localhost/info2180-lab5/world.php",  {
+            method: "post",
+        })
+
+        .done(function(response){
+            result.html(response);
+        })
+
+        .fail(function(){
+            alert('There was a problem loading the database!')
+        });  
     })); 
 });
 
--- world.php
+++ world.php
@@ -7,47 +7,47 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$search_value = $_GET['country'];
+$country = (isset($_GET['country']) ? $_GET['country']:null);
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+	$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+	$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%{$search_value}%'");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-$output = '';
-
-if(mysql_num_rows($stmt) > 0 ){
-	$output = '
-		<table class="CountryList">
-			<tr>
-			<th>Name</th>
-			<th>Continent</th>
-			<th>Independence</th>
-			<th>Head of State</th>
-		</tr>';
-
-		while($row = mysql_fetch_assoc($stmt))
-		{
-			$output .= "
-				<tr>
-					<td>{$row['name']}</td>
-					<td>{$row['continent']}</td>
-					<td>{$row['independence_year']}</td>
-					<td>{$row['head_of_state']}</td>
-				</tr>"
-		}
-$output .= "</table>";
-
-mysql_close($conn);
-
-echo $output;
-}
-
-else
-{
-
-}
 ?>
 
+<table class="CountryList">
+	<caption>List of Countries</caption>
+	<tr>
+		<th>Name</th>
+		<th>Continent</th>
+		<th>Independence</th>
+		<th>Head of State</th>
+	</tr>
 
+	<?php foreach ($results as $row):?>
+		<tr>
+			<td><?= $row['name'];?></td>
+			<td><?= $row['continent'];?></td>
+			<td><?= $row['independence_year'];?></td>
+			<td><?= $row['head_of_state'];?></td>
+		</tr>
+	<?php endforeach; ?>
+</table>
+
+<br></br>
+
+<table class="CityList">
+	<tr>
+		<th>Name</th>
+		<th>District</th>
+		<th>Population</th>
+	</tr>
+
+	<?php foreach ($results as $row):?>
+		<tr>
+			<td><?= $row['name'];?></td>
+			<td><?= $row['district'];?></td>
+			<td><?= $row['population'];?></td>
+		</tr>
+	<?php endforeach; ?>
+</table>
